30605e4aa8753fa664dfa9a4b98ce0fb74884452
Ashley Caselli <ashley.caselli@unige.ch>
2024-05-23 17:40:27 +0200

    test(resources): update rectangle.js code with new syntax for Java-JavaScript interoperability

--- src/test/resources/js/tests/rules/rectangle.js
+++ src/test/resources/js/tests/rules/rectangle.js
@@ -3,14 +3,14 @@ const NS = "http://datashapes.org/js/tests/rules/rectangle.test#";
 function computeArea($this) {
     let width = getProperty($this, "width");
     let height = getProperty($this, "height");
-    let area = TermFactory.literal(width.lex * height.lex, width.datatype);
+    let area = TermFactory.literal(width.getLex() * height.getLex(), width.getDatatype());
     let areaProperty = TermFactory.namedNode(NS + "area");
     return [ [$this, areaProperty, area] ];
 }
 
 function getProperty($this, name) {
     let it = $data.find($this, TermFactory.namedNode(NS + name), null);
-    let result = it.next().object;
+    let result = it.next().getObject();
     it.close();
     return result;
 }
